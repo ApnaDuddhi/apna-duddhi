8ab8d1a345fc4998bfae344776c89469aa6e4b06
SunilKuYadav <sunil.yadav@moonshinetechnology.com>
2024-12-06 18:20:05 +0530

    FEAT: youtube section

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 
 import "./style.css";
-import { About, Header, InspiringQuotes } from "./components";
+import { About, Header, InspiringQuotes, Youtube } from "./components";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <InspiringQuotes />
+      <div style={{border: '2px solid pink'}}>
+        <Youtube />
+      </div>
       <About />
       {/* <p>home</p> */}
+
     </div>
   );
 };
