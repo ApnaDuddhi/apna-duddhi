43ba6d06215a3cae48ee0168f18ee9dd4fdb86e0
SunilKuYadav <sunil.yadav@moonshinetechnology.com>
2024-12-22 16:18:59 +0530

    FIX: lint issue

--- src/pages/home/components/gov/index.tsx
+++ src/pages/home/components/gov/index.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { villages } from "./helper";
 import "./styles.scss";
 
-const website = [{
-  title: "",
-  url: ""
-}]
+// const website = [{
+//   title: "",
+//   url: ""
+// }]
 
 const ApnaDuddhi = () => {
   return (
